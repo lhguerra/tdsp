8d66d36719adb717cf02b0f591cc3c173b225606
Luiz Henrique Guerra <lhguerra@gmail.com>
2022-04-14 10:54:05 -0300

    chore: install app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Gym from './components/gym/Gym';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>The Doing-stuff Project</h1>
+      </header>
+      <Gym />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/gym/Gym.tsx
@@ -0,0 +1,8 @@
+import Threadmil from "./Threadmil";
+
+const Gym = () => <div>
+  <h2>Gym</h2>
+  <Threadmil />
+</div>;
+
+export default Gym;
