3d2730c90cc89181c73fd91160d448740359465c
Luiz Henrique Guerra <lhguerra@gmail.com>
2022-04-25 22:53:27 -0300

    feat: add threadmil chart

--- src/components/gym/Threadmil.tsx
+++ src/components/gym/Threadmil.tsx
@@ -1,36 +1,28 @@
+import Highcharts from 'highcharts/highstock'
+import HighchartsReact from 'highcharts-react-official'
+
+const options = {
+  title: {
+    text: 'Threadmil',
+  },
+  series: [
+    {
+      name: 'time',
+      data: [30.39, 30.02, 30.58, 30.0, 16.25, 17.03, 18.36, 19.14],
+    },
+    {
+      name: 'calories',
+      data: [269, 265, 286, 283, 150, 160, 180, 190],
+    },
+    {
+      name: 'distance',
+      data: [3.84, 3.78, 4.09, 4.03, 2.14, 2.28, 2.56, 2.7],
+    },
+  ],
+}
+
 const Threadmil = () => (
-  <div>
-    <ResponsiveLine
-      curve="monotoneX"
-      data={[
-        {
-          id: 'fake corp. A',
-          data: [
-            { x: 0, y: 7 },
-            { x: 1, y: 5 },
-            { x: 2, y: 11 },
-            { x: 3, y: 9 },
-            { x: 4, y: 13 },
-            { x: 7, y: 16 },
-            { x: 9, y: 12 },
-          ],
-        },
-      ]}
-      xScale={{
-        type: 'linear',
-        min: 0,
-        max: 'auto',
-      }}
-      axisLeft={{
-        legend: 'linear scale',
-        legendOffset: 12,
-      }}
-      axisBottom={{
-        legend: 'linear scale',
-        legendOffset: -12,
-      }}
-    />
-  </div>
+  <HighchartsReact highcharts={Highcharts} options={options} />
 )
 
 export default Threadmil
