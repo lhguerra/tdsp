fe12e351027b6aceedc0ee155de640f99277510f
Luiz Henrique Guerra <lhguerra@gmail.com>
2022-05-07 15:20:53 -0300

    chore: create data source

--- src/components/gym/Threadmil.tsx
+++ src/components/gym/Threadmil.tsx
@@ -1,28 +1,26 @@
-import Highcharts from 'highcharts/highstock'
-import HighchartsReact from 'highcharts-react-official'
+import Highcharts from "highcharts/highstock";
+import HighchartsReact from "highcharts-react-official";
+import threadmilData, { ThreadmilDTO } from "../../data/threadmil";
+
+type ThreadmilSerie = {
+  name: string;
+  data: number[];
+};
+
+const keys: (keyof ThreadmilDTO)[] = ["time", "calories", "distance"];
+
+const normalizeSeries = (data: ThreadmilDTO[]): ThreadmilSerie[] =>
+  keys.map(key => ({ name: key, data: data.map(d => Number(d[key])) }));
 
 const options = {
   title: {
-    text: 'Threadmil',
+    text: "Threadmil",
   },
-  series: [
-    {
-      name: 'time',
-      data: [30.39, 30.02, 30.58, 30.0, 16.25, 17.03, 18.36, 19.14],
-    },
-    {
-      name: 'calories',
-      data: [269, 265, 286, 283, 150, 160, 180, 190],
-    },
-    {
-      name: 'distance',
-      data: [3.84, 3.78, 4.09, 4.03, 2.14, 2.28, 2.56, 2.7],
-    },
-  ],
-}
+  series: normalizeSeries(threadmilData),
+};
 
 const Threadmil = () => (
   <HighchartsReact highcharts={Highcharts} options={options} />
-)
+);
 
-export default Threadmil
+export default Threadmil;
---
+++ src/data/threadmil.ts
@@ -0,0 +1,68 @@
+export type ThreadmilDTO = {
+  id: string;
+  time: string;
+  calories: string;
+  distance: string;
+  date: string;
+};
+
+const data: ThreadmilDTO[] = [
+  {
+    id: "1",
+    time: "30.39",
+    calories: "269",
+    distance: "3.84",
+    date: "12/12/2021",
+  },
+  {
+    id: "2",
+    time: "30.02",
+    calories: "265",
+    distance: "3.78",
+    date: "13/12/2021",
+  },
+  {
+    id: "3",
+    time: "30.58",
+    calories: "286",
+    distance: "4.09",
+    date: "14/12/2021",
+  },
+  {
+    id: "4",
+    time: "30.0",
+    calories: "283",
+    distance: "4.03",
+    date: "18/12/2021",
+  },
+  {
+    id: "5",
+    time: "16.25",
+    calories: "150",
+    distance: "2.14",
+    date: "28/03/2022",
+  },
+  {
+    id: "6",
+    time: "17.03",
+    calories: "160",
+    distance: "2.28",
+    date: "29/03/2022",
+  },
+  {
+    id: "7",
+    time: "18.36",
+    calories: "180",
+    distance: "2.56",
+    date: "04/04/2022",
+  },
+  {
+    id: "8",
+    time: "19.14",
+    calories: "190",
+    distance: "2.7",
+    date: "05/04/2022",
+  },
+];
+
+export default data;
